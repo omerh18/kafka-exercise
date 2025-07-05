e1a5187e0dc58ba678f0422687a1de55346ab392
omerh18 <omerh18@gmail.com>
2025-07-06 00:34:10 +0300

    added generation script of a candidate exercise folder

---
+++ exercise_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker-compose up -d
+
+docker exec -it $(docker ps | grep 'bitnami/kafka' | awk '{print $1}') kafka-topics.sh --create \
+  --topic my-topic \
+  --bootstrap-server localhost:9092 \
+  --partitions 100 \
+  --replication-factor 1
+
+docker load -i my-kafka-producer.tar
+
+docker load -i my-kafka-consumer.tar
+
+docker run -d --rm --network kafka_kafka-net -e EXEC_ENV=prod -p 8000:8000 my-kafka-producer:1.0
+
+docker run -d --rm --network kafka_kafka-net -e EXEC_ENV=prod -p 8001:8001 my-kafka-consumer:1.0
+
---
+++ generate_candidate_exercise_directory.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir for_candidate
+
+cp prometheus.yml for_candidate/
+
+cp docker-compose.yml for_candidate/
+
+cp exercise_setup.sh for_candidate/
+
+chmod +x for_candidate/exercise_setup.sh
+
+cd producer_consumer_app
+
+docker build -f producer/Dockerfile -t my-kafka-producer:1.0 .
+
+docker build -f consumer/Dockerfile -t my-kafka-consumer:1.0 .
+
+cd ..
+
+docker save -o for_candidate/my-kafka-producer.tar my-kafka-producer:1.0
+
+docker save -o for_candidate/my-kafka-consumer.tar my-kafka-consumer:1.0
+
